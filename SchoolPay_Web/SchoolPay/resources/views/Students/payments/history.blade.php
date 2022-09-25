5c2ba8065b2003592451e0b35235d27c050fed96
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2022-09-25 19:32:12 +0100

    Fix Bug: add content on student.payments.history route

---
+++ SchoolPay_Web/SchoolPay/resources/views/Students/payments/history.blade.php
@@ -0,0 +1,11 @@
+<x-dashboard-layout-student>
+
+    <x-slot name="title">
+        <h1 class="font-extrabold text-2xl p-2 color-blue">
+            Historique des paiements
+        </h1>
+    </x-slot>
+
+
+	
+</x-dashboard-layout-student>
